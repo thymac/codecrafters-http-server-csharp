3eab2fb97a1027b1f46ff7a3c3a5397c3f0b65b8
thymac <thywillberdone@gmail.com>
2024-07-15 11:35:34 +0300

    Respond with 200

--- src/Server.cs
+++ src/Server.cs
@@ -9,11 +9,18 @@ using System.Text;
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 // server.AcceptSocket(); // wait for client
+Console.WriteLine($"Started server");
 
-using (Socket clientSocket = server.AcceptSocket())
-{
-  byte[] responseBytes = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-  clientSocket.Send(responseBytes);
+try {
+  // server.AcceptSocket(); // wait for client
+  var client = await server.AcceptTcpClientAsync();
+  Console.WriteLine($"Accepted TcpClient");
+  var stream = client.GetStream();
+  var message = $"HTTP/1.1 200 OK\r\n\r\n";
+  var encodedMessage = Encoding.UTF8.GetBytes(message);
+  await stream.WriteAsync(encodedMessage);
+  Console.WriteLine($"Responded message: \"{message}\"");
+} finally {
+  Console.WriteLine($"Stopping server");
+  server.Stop();
 }
-
-server.Stop();
