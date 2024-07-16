e311129c39bc9a30fe8e592c5486c292df2f6cd2
thymac <thywillberdone@gmail.com>
2024-07-16 22:44:58 +0300

    Extract URL path

--- src/Server.cs
+++ src/Server.cs
@@ -1,36 +1,82 @@
+using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// Console.WriteLine("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
+// 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-// server.AcceptSocket(); // wait for client
 Console.WriteLine($"Started server");
 
 try
 {
-  // Accept a client connection
-  TcpClient client = await server.AcceptTcpClientAsync();
-  Console.WriteLine("Accepted TcpClient");
-
-  using (NetworkStream stream = client.GetStream())
+  while (true)
   {
-    // Prepare the HTTP response
-    string message = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
-    byte[] encodedMessage = Encoding.UTF8.GetBytes(message);
-    // Send the HTTP response to the client
-    await stream.WriteAsync(encodedMessage, 0, encodedMessage.Length);
-    Console.WriteLine($"Responded message: \"{message}\"");
-  }
+    // Accept a client connection
+    TcpClient client = await server.AcceptTcpClientAsync();
+    Console.WriteLine("Accepted TcpClient");
 
-  client.Close();
+    // Handle the client connection asynchronously
+    await HandleClientAsync(client);
+  }
 }
 finally
 {
   Console.WriteLine($"Stopping server");
   server.Stop();
+}
+
+static async Task HandleClientAsync(TcpClient client)
+{
+  using NetworkStream stream = client.GetStream();
+  using StreamWriter writer = new StreamWriter(stream, new UTF8Encoding(false)) { NewLine = "\r\n", AutoFlash = true };
+  using StreamReader reader = new StreamReader(stream);
+
+  // Read the HTTP request line
+  string requestLine = await reader.ReadLineAsync();
+  if (string.IsNullOrEmpty(requestLine))
+  {
+    client.Close();
+    return;
+  }
+  Console.WriteLine($"Received request: {requestLine}");
+
+  // Parse the request line
+  string[] requestParts = requestLine.Split(' ');
+  string method = requestParts[0];
+  string path = requestParts[1];
+  string httpVersion = requestParts[2];
+
+  // Validate the request
+  if (requestParts.Length != 3)
+  {
+    await SendResponseAsync(writer, "400 Bad Request");
+    client.Close();
+    return;
+  }
+  if (method != "GET" || (httpVersion != "HTTP/1.1" && httpVersion != "HTTP/1.0"))
+  {
+    await SendResponseAsync(writer, "400 Bad Request");
+    client.Close();
+    return;
+  }
+
+  // Send appropriate responses
+  if (path == "/")
+  {
+    await SendResponseAsync(writer, "200 OK");
+  }
+  else
+  {
+    await SendResponseAsync(writer, "404 Not Found");
+  }
+
+  client.Close();
+}
+
+static async Task SendResponseAsync(StreamWriter writer, string statusCode)
+{
+  await writer.WriteLineAsync($"HTTP/1.1 {statusCode}\r\n");
 }
